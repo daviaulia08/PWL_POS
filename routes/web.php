16cbd1dfc00218e1052707eb12a4797b7e280996
Davi Aulia Maghfirah <davi.auliaaaa@gmail.com>
2026-02-23 02:10:03 +0700

    JS3_Praktikum 6 : Implementasi Eloquent ORM

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
@@ -11,3 +12,4 @@ Route::get('/', function () {
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
