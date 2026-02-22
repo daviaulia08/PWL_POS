2f6e6e6ee35a185d1d91e06cb37b65fa1baecadb
Davi Aulia Maghfirah <davi.auliaaaa@gmail.com>
2026-02-23 01:24:49 +0700

    JS3_Praktikum 5 : Implementasi Query Builder

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
